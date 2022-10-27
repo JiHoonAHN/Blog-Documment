64c3acd4c324b2b85b5bad02994473c0f98711ee
JiHoonAHN <ahnjh1028@naver.com>
2022-10-27 17:04:14 +0900

    Create ExRIB

---
+++ Architecture/RIBs/RIBs-1/ExRIBs/ExRIBs/Sources/Application/SceneDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+import RIBs
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    private var launchRouter: LaunchRouting?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+              let window = UIWindow(windowScene: windowScene)
+              self.window = window
+
+              let launchRouter = RootBuilder(dependency: AppComponent()).build()
+              self.launchRouter = launchRouter
+              launchRouter.launch(from: window)
+          }
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    func sceneWillResignActive(_ scene: UIScene) {}
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
